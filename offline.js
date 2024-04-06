2574cbb426745c2abbc201fe4ffd78fbca1453b3
praveen ^_^ <praveenin321@gmail.com>
2024-04-06 23:03:41 +0530

    first commit

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1712424418,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background_obj-sheet0.png",
+		"images/box_obj-sheet0.png",
+		"images/praveen_obj-sheet0.png",
+		"images/praveen_obj-sheet1.png",
+		"images/scoreboardbg-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/gameover_obj-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/button-sheet0.png",
+		"images/praveen_start-sheet0.png",
+		"images/praveen_start-sheet1.png",
+		"images/sprite3-sheet0.png",
+		"media/bounce2.ogg",
+		"media/click.ogg",
+		"media/music.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
